ad19e67654429117fd9714a8d25c31c51e345ee0
Llyme <InfiniteX777@gmail.com>
2019-11-16 17:54:57 +0800

    Initial commit.

---
+++ Hooks.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace AI_PovX
+{
+	public partial class AI_PovX
+	{
+		[HarmonyPrefix, HarmonyPatch(typeof(NeckLookControllerVer2), "LateUpdate")]
+		public static bool Prefix_NeckLookControllerVer2_LateUpdate(NeckLookControllerVer2 __instance)
+		{
+			if (Manager.Housing.Instance.IsCraft ||
+				!Controller.toggled ||
+				Controller.chaCtrl == null)
+				return true;
+
+			if (Controller.focus == 0 && !Tools.IsHScene())
+				Controller.FreeRoamPoV();
+			else
+				Controller.ScenePoV();
+
+			return false;
+		}
+	}
+}
